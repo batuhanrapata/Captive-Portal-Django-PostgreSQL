431fc8fb69507f2b8a56b3273ebd0907dffe2e95
batuhan rapata <56839590+batuhanrapata@users.noreply.github.com>
2023-02-09 17:31:07 +0300

    captiveportalstart düzenlendi

--- firewall_uygulama/captive_portal_start.py
+++ firewall_uygulama/captive_portal_start.py
@@ -6,18 +6,18 @@ PORT = 9090  # the port in which the captive portal web server listens
 IFACE = "wlan2"  # the interface that captive portal protects//??
 IP_ADDRESS = "172.16.0.1"  # the ip address of the captive portal (it can be the IP of IFACE)//network ip ile eşleşmeli!
 
-subprocess.call4(
-        ["iptables", "-A", "FORWARD", "-i", IFACE, "-p", "tcp", "--dport", "53", "-j", "ACCEPT"])  # dns portu
+subprocess.call(
+    ["iptables", "-A", "FORWARD", "-i", IFACE, "-p", "tcp", "--dport", "53", "-j", "ACCEPT"])  # dns portu
 subprocess.call(["iptables", "-A", "FORWARD", "-i", IFACE, "-p", "udp", "--dport", "53", "-j", "ACCEPT"])
 subprocess.call(
-        ["iptables", "-A", "FORWARD", "-i", IFACE, "-p", "tcp", "--dport", str(PORT), "-d", IP_ADDRESS, "-j",
-         "ACCEPT"])  # captive portal portu
+    ["iptables", "-A", "FORWARD", "-i", IFACE, "-p", "tcp", "--dport", str(PORT), "-d", IP_ADDRESS, "-j",
+     "ACCEPT"])  # captive portal portu
 subprocess.call(["iptables", "-A", "FORWARD", "-i", IFACE, "-j", "DROP"])  # diğer tüm paketler iptal edilir
 httpd = HTTPServer(('', PORT), http.server.SimpleHTTPRequestHandler)  # captive portal başlatılır
 subprocess.call(
-        ["iptables", "-t", "nat", "-A", "PREROUTING", "-i", IFACE, "-p", "tcp", "--dport", "80", "-j", "DNAT",
-         "--to-destination", IP_ADDRESS + ":" + str(PORT)])  # iptables ayarları yapılır
+    ["iptables", "-t", "nat", "-A", "PREROUTING", "-i", IFACE, "-p", "tcp", "--dport", "80", "-j", "DNAT",
+     "--to-destination", IP_ADDRESS + ":" + str(PORT)])  # iptables ayarları yapılır
 try:  # captive portal başlatılır
-        httpd.serve_forever()
+    httpd.serve_forever()
 except KeyboardInterrupt:
-     pass
+    pass
